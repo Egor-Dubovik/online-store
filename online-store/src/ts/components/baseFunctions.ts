237edff49110155e7be8fd8acb8ff8177662110f
Egor Dubovik <egorka54896@gmail.com>
2022-07-13 15:37:12 +0300

    feat: add baseFunctions (toggleClasses, removeClasses) and numbers.ts

---
+++ online-store/src/ts/components/baseFunctions.ts
@@ -0,0 +1,13 @@
+import { INITIAL_STEP } from '../constants/numbers';
+
+export function toggleClasses(array: HTMLElement[], className: string): void {
+  for (let i = INITIAL_STEP; i < array.length; i++) {
+    array[i].classList.toggle(className);
+  }
+}
+
+export function removeClasses(array: HTMLElement[], className: string): void {
+  for (let i = INITIAL_STEP; i < array.length; i++) {
+    array[i].classList.remove(className);
+  }
+}
---
+++ online-store/src/ts/constants/numbers.ts
@@ -0,0 +1,11 @@
+export enum Numbers {
+  halfUnit = 0.5,
+  zero = 0,
+  one = 1,
+  two = 2,
+  three = 3,
+  four = 4,
+  ten = 10,
+}
+
+export const INITIAL_STEP = 0;
