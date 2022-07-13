714ef0ec4ac99d35c79f0fe4c4e6bdaffa0e6a4d
Egor Dubovik <egorka54896@gmail.com>
2022-07-13 15:39:29 +0300

    feat: create functions (toggleInputSearch, toggleBurgerMenu)

--- online-store/src/ts/index.ts
+++ online-store/src/ts/index.ts
@@ -1,5 +1,20 @@
 import '../scss/style.scss';
 // import json from '../assets/products.json';
+import { removeClasses, toggleClasses } from './components/baseFunctions';
 
+const searchButton = document.querySelector('.header-search__button') as HTMLButtonElement;
+const navigationBlock = document.querySelector('.header__navigation') as HTMLElement;
+const menuButton = document.querySelector('.icon-menu') as HTMLButtonElement;
+const searchForm = document.querySelector('.header__search-form') as HTMLFormElement;
+const infoList = document.querySelector('.header-top__info-list') as HTMLElement;
 
-// console.log('HHH', json);
+const toggleInputSearch = (): void => {
+  toggleClasses([searchForm, searchButton], '_active');
+  navigationBlock.classList.toggle('_hidden');
+};
+
+const toggleBurgerMenu = (): void => {
+  toggleClasses([menuButton, navigationBlock], '_active');
+  removeClasses([searchForm, searchButton], '_active');
+  navigationBlock.classList.remove('_hidden');
+};
