b4582bd878706895ba62df8cb2b3d150face13b8
Egor Dubovik <egorka54896@gmail.com>
2023-01-23 21:44:30 +0300

    feat: deploy production

--- online-store/src/interfaces/storage.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IStorage {
-  set: (key: string, value: string) => void;
-  remove: (key: string) => void;
-  get: (key: string) => string;
-}
--- online-store/src/types/card.types.ts
+++
@@ -1,8 +0,0 @@
-export type AvailabilityOptions = {
-  text: string;
-  icon: string;
-  basketClass: string;
-};
-
-export type SortingType = 'year' | 'price';
-export type FilteringType = 'price' | 'rating' | 'brend' | 'popular';
